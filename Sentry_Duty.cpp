fc264ca5dc1d5c2fcf794ec4953c176397dbc2fc
EmanMuhammed <emanstagequri4212016@gmail.com>
2017-12-11 10:33:05 +0300

    add stop condition

--- Sentry_Duty.cpp
+++ Sentry_Duty.cpp
@@ -4,21 +4,24 @@ using namespace std;
 main()
 {   
    
-   string week[7]={"Friday","Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday"};
+   string week[7]={"Friday","Saturday","Sunday","Monday","Tuesday","Wednesday","Thursday"} ,day;
    
-   int startSentryDuty,numberDays,sentryDutyNumber,storage=0;
+   int startSentryDuty,numberDays,sentryDutyNumber,storage=0,stopCondition;
    while(1)
    {
-   
-       cout<<"Enter number (0-6) start whirlpool:";
-       cin>>startSentryDuty;
-   
-       cout<<"How many times you have whirlpool";
+       cout<<"Enter a start day of Sentry duty :";
+       cin>>day;
+          
+       cout<<"How many times are repeated your Sentry duty ";
        cin>>numberDays;
    
-       cout<<"Enter number of whirlpool";
+       cout<<"Enter number of Sentry duty if you know the start Sentry duty is "<<day<<": ";
        cin>>sentryDutyNumber;
-   
+       
+       for(int i=0;i<7;i++)
+           if(day==week[i])
+              startSentryDuty=i;
+           
        startSentryDuty++;
    
        for(int y=0;y<(sentryDutyNumber-1);y++)
@@ -30,6 +33,14 @@ main()
 	    }  
 
         cout<<week[storage]<<"  "<<storage<<endl;
+        
+        cout<<"Enter 0 to Exit 1 to continue :";
+        cin>>stopCondition;
+        if(stopCondition==0)
+           break;
+        else if(stopCondition==1)
+		   continue;          
+        
    }
    
 getch();
